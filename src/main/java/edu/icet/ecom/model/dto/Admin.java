b371c6af683661801670e564209d605b97e56284
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-11-15 08:42:31 +0530

    create dtos and entities for admin,member,payment,schedule,trainer and user

---
+++ src/main/java/edu/icet/ecom/model/dto/Admin.java
@@ -0,0 +1,15 @@
+package edu.icet.ecom.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Admin {
+
+    private String adminId;
+    private String name;
+    private String email;
+}
